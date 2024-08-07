c63a798d6aa349d7f8f6e2e21d433fb2003112a6
abdulmateenGit <abdulmateen.lhr00@gmail.com>
2024-08-07 11:07:34 -0700

    setup

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import ColorList from "@/components/ColorList";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <ColorList color="#0891b2" />
+    </View>
+  );
+}
---
+++ components/ColorList.tsx
@@ -0,0 +1,33 @@
+import { View, Text, Image, ScrollView, StyleSheet } from 'react-native'
+import React from 'react'
+import { opacity } from 'react-native-reanimated/lib/typescript/reanimated2/Colors'
+
+export default function ColorList({ color }) {
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      {
+        [1, 0.8, 0.5].map(opacity => (
+          <View
+            key={opacity}
+            style={[styles.color, { backgroundColor: color, opacity }]}
+          />
+        ))
+      }
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  color: {
+    width: '100%',
+    height: 150,
+    borderRadius: 25,
+    borderCurve: 'continuous',
+    marginBottom: 15,
+  },
+  container: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    height: '100%'
+  }
+})
